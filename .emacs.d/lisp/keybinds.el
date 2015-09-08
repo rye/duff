71b42c84ab74ac8edea4a9f0649f2bedc22bdbf2
Kristofer Rye <kristofer.rye@gmail.com>
2015-09-08 09:06:34 -0500

    Add interface theme switcher keybind.

--- .emacs.d/lisp/keybinds.el
+++ .emacs.d/lisp/keybinds.el
@@ -4,6 +4,7 @@
 (global-set-key (kbd "M-%") 'query-replace-regexp)
 
 (global-set-key (kbd "C-x g") 'goto-line)
+(global-set-key (kbd "C-x t s") 'interface-theme-switcher--change-to)
 
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
