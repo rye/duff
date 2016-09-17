a923c8dff312513bd45df55d0eec58f4b1600325
Kristofer Rye <kristofer.rye@gmail.com>
2016-09-17 10:07:23 -0500

    Emacs: Re-align completion so that it's a little bit easier to read

--- .emacs.d/lisp/completion.el
+++ .emacs.d/lisp/completion.el
@@ -8,16 +8,20 @@
 (if (require 'smex nil 'no-error)
     (progn
       (smex-initialize)
-      (global-set-key [(meta x)] (lambda ()
-                                   (interactive)
-                                   (or (boundp 'smex-cache)
-                                       (smex-initialize))
-                                   (global-set-key [(meta x)] 'smex)
-                                   (smex)))
+      (global-set-key
+       [(meta x)]
+       (lambda ()
+         (interactive)
+         (or (boundp 'smex-cache)
+             (smex-initialize))
+         (global-set-key [(meta x)] 'smex)
+         (smex)))
 
-      (global-set-key [(shift meta x)] (lambda ()
-                                         (interactive)
-                                         (or (boundp 'smex-cache)
-                                             (smex-initialize))
-                                         (global-set-key [(shift meta x)] 'smex-major-mode-commands)
-                                         (smex-major-mode-commands)))))
+      (global-set-key
+       [(shift meta x)]
+       (lambda ()
+         (interactive)
+         (or (boundp 'smex-cache)
+             (smex-initialize))
+         (global-set-key [(shift meta x)] 'smex-major-mode-commands)
+         (smex-major-mode-commands)))))
